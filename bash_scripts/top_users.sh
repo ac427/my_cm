f746f9a6b4b2c0c852d3a74090df401edeaf0952
memoverflow <memoverflow@gmail.com>
2016-02-04 11:14:47 -0500

    modifiled date

--- bash_scripts/top_users.sh
+++ bash_scripts/top_users.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-LOG="top_users.$(date +%F).log"
-echo $LOG
-date >> $LOG
+mkdir -p /home/$USER/log 
+LOG="/home/$USER/log/top_users.$(date +%F).log"
+date +%m%d%y_%H_%M >> $LOG
 top icb -n 1 | grep -A 10 USER >> $LOG
 
