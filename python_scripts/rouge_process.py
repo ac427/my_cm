707cd78ade8ee7d3284945827d94ee12850288c2
Ananta Chakravartula <ananta_chakravartula@hms.harvard.edu>
2016-04-05 15:41:56 -0400

    working program

--- python_scripts/rouge_process.py
+++ python_scripts/rouge_process.py
@@ -1,22 +1,66 @@
-#!/bin/env python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  rouge_process.py
+#  
+#  Copyright 2016 Ananta Chakravartula <ac427@newton>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+# 
+"""
+Script to find rogue process on compute nodes which bypassed LSF scheduler
+"""
+
+
+
 import subprocess
 import re
 
 PIDS=[];
-
-#cut -d:  -f1 /etc/passwd
+WHITELIST=['1']
+LSFPIDS=[]
+# get  all local account ; cut -d:  -f1 /etc/passwd
 local_users_cmd=['cut', '-d:', '-f1', '/etc/passwd']
 stdout=subprocess.Popen(local_users_cmd,stdout=subprocess.PIPE)
 users=stdout.communicate()[0].replace('\n','|').rsplit('|',1)[0]
-process_cmd=["ps -ef | awk 'NR>1' | egrep -v \""+users+" \"| awk '{print $3}'" ]
-directory_user_process=subprocess.Popen(process_cmd,stdout=subprocess.PIPE,shell=True)
-#directory_user_process.stdin.write("egrep -v \"" + users+"\"")
-process=directory_user_process.communicate()[0].strip()
-for i in process.split():
-	try:
-		PIDS.append(re.search(r'\d+',i).group())
-	except AttributeError:
-		pass 
-#print users.replace('\n','|').rsplit('|',1)[0]
-# find ppid of 2426; ps -p 2426 -o ppid=
-print PIDS
+# get all pids excluding local accounts and 68 
+process_cmd=["ps -ef | awk 'NR>1' | egrep -v \"68|"+users+" \"| awk '{print $3}' | sort -u" ]
+process=directory_user_process=subprocess.Popen(process_cmd,stdout=subprocess.PIPE,shell=True).communicate()[0].strip()
+
+## convert all pids to list 
+for pid in process.split():
+	PIDS.append(pid)
+
+# get lsf process pids
+
+LSF_PIDS=subprocess.Popen("service lsf status |   grep -o '[0-9]*'",stdout=subprocess.PIPE,shell=True).communicate()[0].strip()
+for pid in LSF_PIDS.split():
+	WHITELIST.append(pid)
+	LSFPIDS.append(pid)
+
+# get all the child and grand[grand*]-child process and put it in white list
+for pid in LSFPIDS:
+	for pid in subprocess.Popen('pstree -np '+pid+' | grep -o "[0-9]*"',stdout=subprocess.PIPE,shell=True).communicate()[0].strip().split():
+		WHITELIST.append(pid)
+
+# find rouge process
+
+for pid in PIDS:
+	if pid in WHITELIST:
+		pass
+	else:
+		print "rougue pid " + pid
