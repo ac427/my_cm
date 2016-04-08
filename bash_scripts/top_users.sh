99ff96d486f2aa0e912863be9b740a90cb005eb8
Ananta Chakravartula <ananta_chakravartula@hms.harvard.edu>
2016-04-07 21:37:56 -0400

    modfied to work on orchestra

--- bash_scripts/top_users.sh
+++ bash_scripts/top_users.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-mkdir -p /home/$USER/log 
-LOG="/home/$USER/log/top_users.$(date +%F).log"
-date +%m%d%y_%H_%M >> $LOG
-top icb -n 1 | grep -A 10 USER >> $LOG
+. /home/ac427/.bashrc
 
+LOG="/home/ac427/log/top_users.$(date +%F).$(hostname).log"
+echo $LOG
+date >> $LOG
+/usr/bin/top -cbn 1 | head -20 | grep -A 10 COMMAND >> $LOG
