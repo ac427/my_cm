7c7e1e0ec1d5895135a5383241db27c1d62b4ac0
ac <ac@techsquare.com>
2016-06-16 16:11:59 -0400

    not sure what I did

--- slurm/gpu_count.sh
+++ slurm/gpu_count.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
-total_gpus=$(sinfo -o "%D %G" -t "idle,mixed,alloc,comp" | grep gpu | awk '{print $1}')
+input=$1
+total_gpus=$(sinfo -o "%D %G" -t "idle,mixed,alloc,comp" | grep gpu | grep ":$input" | awk '{print $1}')
 jobs_with_gpus_res=$(for i in $(squeue -h -o "%i" -t "running");do scontrol show job $i | grep "Gres=gpu" | awk -F'gpu:' '{print $2}' | awk '{print $1}';done | awk '{sum+=$1}END{print sum}')
-gpu_jobs=$(for i in $(squeue -h -o "%i" -t "running");do scontrol show job $i | grep gpu > /dev/null; if [ $? -eq 0 ] ; then scontrol show job $i | grep "NodeList" | grep -v "ReqNode" | awk -F'=' '{print $2}'  ;fi; done | tr "\n" "\t") 
-nodes_that_can_take_jobs=$(sinfo -o "%N %G" -t "idle,mixed" | grep gpu  | awk '{print $1}' )
+gpu_jobs=$(for i in $(squeue -h -o "%i" -t "running");do scontrol show job $i | grep "Gres=gpu" > /dev/null; if [ $? -eq 0 ] ; then scontrol show job $i | grep "NodeList" | grep -v "ReqNode" | awk -F'=' '{print $2}'  ;fi; done | tr "\n" "\t") 
+nodes_that_can_take_jobs=$(sinfo -o "%N %G" -t "idle,mixed" | grep gpu  | grep ":$input" | awk '{print $1}' ) 
 #nodes_that_can_take_jobs=$(sinfo -o "%D %G" -t "idle,mixed" | grep gpu | awk '{print $1}')
 echo "total gpus   $total_gpus "
 echo "nodes that are having gpus jobs $gpu_jobs" 
