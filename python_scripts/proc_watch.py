abd3bb8e613bf2c0ae3c50f7662d6c7401f39d45
ac <ac@techsquare.com>
2016-08-16 14:49:05 -0400

    fixed bugs

--- python_scripts/proc_watch.py
+++ python_scripts/proc_watch.py
@@ -14,7 +14,7 @@ def proc_watch():
 	while True:
 		top_cmd="top -b -n 1 | sed -n '8,17p'| egrep -v \" "+ WHITE_LIST_STR + "\" |  awk '{print $1}' "
 		top_pids=subprocess.Popen(top_cmd,stdout=subprocess.PIPE,shell=True).communicate()[0].strip().split()
-		time.sleep(15)
+		time.sleep(900)
 		count=0
 		for pid in top_pids:
 			pid_info=["top -b -n 1 -p "+pid+"| sed -n '8,17p'"]
@@ -23,20 +23,18 @@ def proc_watch():
 				cpu_pid = rogue_info.communicate()[0].split()
 				try:
 					if float(cpu_pid[8]) > 90:
-						print "found pid > 10"
 						s=smtplib.SMTP('localhost')
-						s.set_debuglevel(1)
-	       					body=time.strftime("%d/%m/%Y-%H:%M:%S")
-       						body+="\n"+cpu_pid 
+#						s.set_debuglevel(1)
+	       					body=time.strftime("%d/%m/%Y-%H:%M:%S")+"\n"
+	       					body+='\t'.join(cpu_pid) 
 						msg = MIMEText(body)
 						sender = 'engaging-admin@techsquare.com'
-						recipients = ['ananta.c@gmail.com']
-						msg['Subject'] = "rogue pid" + cpu_pid[0]
+						recipients = ['ac@techsquare.com']
+						msg['Subject'] = "rogue pid" +cpu_pid[0]
 						msg['From'] = sender
 						msg['To'] = ", ".join(recipients)
 						s.sendmail(sender, recipients, msg.as_string())
 						s.quit()
-						print 'not printing this line'
 				except:
 					pass
 
