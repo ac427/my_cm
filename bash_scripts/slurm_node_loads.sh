58d08f91ead9ec3ea698afa724b0dd97431e270a
ac <ac@techsquare.com>
2017-02-19 08:41:36 -0500

    added email logs

--- bash_scripts/slurm_node_loads.sh
+++ bash_scripts/slurm_node_loads.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
 # program to print load higher than total cpu 
 
-FILE=$( mktemp --tmpdir=/tmp )
+TEMPFILE=$( mktemp --tmpdir=/tmp )
+TEMPLOGFILE=$( mktemp --tmpdir=/tmp )
 SPACE=','
 #SPACE='\t '
 
 scontrol show node | egrep "NodeName|CPULoad" |
 #TO PRINT ALL CPU INFO 
-#awk '{ if ($1 ~ /NodeName/) printf "\n %s,",$1; else if ($1 ~ /CPU/) for (i=1;i<=NF;i++)  printf "%s,",$i }' | tee $FILE
+#awk '{ if ($1 ~ /NodeName/) printf "\n %s,",$1; else if ($1 ~ /CPU/) for (i=1;i<=NF;i++)  printf "%s,",$i }' | tee $TEMPFILE
 
 #IF YOU WANT JUST TOTAL CPU AND LOAD 
-awk '{ if ($1 ~ /NodeName/) printf "\n %s,",$1; else if ($1 ~ /CPU/)  printf "%s,%s",$(NF-1),$NF}  END {printf "\n" }' | tee $FILE  > /dev/null
+awk '{ if ($1 ~ /NodeName/) printf "\n %s,",$1; else if ($1 ~ /CPU/)  printf "%s,%s",$(NF-1),$NF}  END {printf "\n" }' | tee -a $TEMPFILE  > /dev/null
 
-for lines in $(cat $FILE | grep -v "N/A");
+for lines in $(cat $TEMPFILE | grep -v "N/A");
 do
-	#x is total cpu 
-	x=$( echo $lines | awk -F, '{print $2}' | awk -F= '{print $2}')
-	#y is load 
-	y=$( echo $lines | awk -F, '{print $3}' | awk -F= '{print $2}')
+	#CPUTot 
+	CPUTot=$( echo $lines | awk -F, '{print $2}' | awk -F= '{print $2}')
+	#CPULoad is load 
+	CPULoad=$( echo $lines | awk -F, '{print $3}' | awk -F= '{print $2}')
 
 	# rounding float to int 
-	inty=${y%.*}
+	intCPULoad=${CPULoad%.*}
 	# incrementing total cpu +1
-	intx=$((x+1))
+	intCPUTot=$((CPUTot+1))
 
-		if [ $inty -ge $intx ];	then
-			echo $lines 
+		if [ $intCPULoad -ge $intCPUTot ];	then
+			echo $lines  | tee -a $TEMPLOGFILE
 		fi
 done
-rm $FILE
+
+$(which mail) -s "Nodes with High CPULoad `hostname`" ac@techsquare.com < ${TEMPLOGFILE}
+
+#echo  "Nodes with High CPULoad" | mail -s "Nodes with High CPULoad" -a $TEMPFILE ac@techsqaure.com 
+rm $TEMPFILE
+rm $TEMPLOGFILE
