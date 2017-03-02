d89feb700238a930646f535406dccdae22f7f235
foo <awkbug@gmail.com>
2017-03-01 21:48:35 -0500

    program to disable ht

---
+++ bash_scripts/disable_ht.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+# program works on both ubuntu and centos; ubuntu has cpus seperted with - and centos ,just put this script in /etc/rc.local
+
+CPU_OFFLINE=$( cat /sys/devices/system/cpu/cpu*/topology/thread_siblings_list | awk -F- '{print $2}' | sort -u) 
+if [ -z "$CPU_OFFLINE" ] ; then
+CPU_OFFLINE=$( cat /sys/devices/system/cpu/cpu*/topology/thread_siblings_list | awk -F, '{print $2}' | sort -u ) 
+fi
+
+for CPU in $CPU_OFFLINE
+do
+echo 0 > /sys/devices/system/cpu/cpu$CPU/online
+done
